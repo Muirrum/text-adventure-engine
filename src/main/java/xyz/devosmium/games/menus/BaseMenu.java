be1c391986899746725670f8d72488bf4d364502
Dev-Osmium <owen@devosmium.xyz>
2019-08-29 16:37:25 -0400

    Make BaseMenu work

--- src/main/java/xyz/devosmium/games/menus/BaseMenu.java
+++ src/main/java/xyz/devosmium/games/menus/BaseMenu.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import xyz.devosmium.games.util.MessageQueue;
+
 public class BaseMenu {
 
     protected List<MenuItem> menuItems = new ArrayList<>();
@@ -17,6 +19,31 @@ public class BaseMenu {
             commandMap.put(menuItem.getCmdKey(), menuItem);
             i++;
         }
-        
+        // Find user input to return
+        return getSelection(menuList);
+    }
+
+    // Get user input from the message queue
+    protected MenuItem getSelection(List<MenuItem> m) {
+        this.printMenuItems(m);
+        String command = MessageQueue.take();
+        if (commandMap.containsKey(command.toLowerCase())) {
+            return commandMap.get(command.toLowerCase());
+        } else {
+            MessageQueue.add("I don't know what '" + command + "' means.");
+            return this.displayMenu(m);
+        }
+    }
+
+    private void printMenuItems(List<MenuItem> m) {
+        int i = 1;
+        for (MenuItem item : m) {
+            if (item.getDescription() != null) {
+                MessageQueue.add("[" + i + "]" + item.getCommand() + " - " + item.getDescription());
+            } else {
+                MessageQueue.add("[" + i + "]" + item.getCommand());
+            }
+            i++;
+        }
     }
 }
