40916b5235b0cad6c9aeeddbbb1c275b3beebec8
Dev-Osmium <owen@devosmium.xyz>
2019-08-29 16:22:33 -0400

    Make main class output something not Hello, WOrld!

--- src/main/java/xyz/devosmium/games/App.java
+++ src/main/java/xyz/devosmium/games/App.java
@@ -1,5 +1,7 @@
 package xyz.devosmium.games;
 
+import xyz.devosmium.games.util.MessageQueue;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MessageQueue.add("test");
     }
 }
