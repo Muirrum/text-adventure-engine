f8322738d0cb3a28db8f234286c0a6fa0c0f7ae5
Dev-Osmium <owen@devosmium.xyz>
2019-08-29 16:22:53 -0400

    Add MessageQueue handler

---
+++ src/main/java/xyz/devosmium/games/util/MessageQueue.java
@@ -0,0 +1,24 @@
+package xyz.devosmium.games.util;
+
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MessageQueue {
+    private static Logger logger = LoggerFactory.getLogger(MessageQueue.class);
+
+    public static void add(String message) {
+        logger.debug("add(" + message + ")");
+        System.out.println(message);
+    }
+
+    public static String take() {
+        String message = null;
+
+        Scanner input = new Scanner(System.in);
+        message = input.nextLine();
+        input.close();
+        return message;
+    }
+}
