9e57ed0aa5fd9634a89994d2b5d8fcb85d3c9444
Dev-Osmium <owen@devosmium.xyz>
2019-08-29 20:58:21 -0400

    Make the menus launch

--- src/main/java/xyz/devosmium/games/App.java
+++ src/main/java/xyz/devosmium/games/App.java
@@ -1,5 +1,9 @@
 package xyz.devosmium.games;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import xyz.devosmium.games.menus.MainMenu;
 import xyz.devosmium.games.util.MessageQueue;
 
 /**
@@ -8,8 +12,10 @@ import xyz.devosmium.games.util.MessageQueue;
  */
 public class App 
 {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
-        MessageQueue.add("test");
+        logger.debug("Starting game");
+        new MainMenu();
     }
 }
