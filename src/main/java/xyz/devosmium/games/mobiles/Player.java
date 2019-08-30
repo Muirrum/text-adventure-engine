abc639b5938659d5af30832a04615b9e18c91145
Dev-Osmium <owen@devosmium.xyz>
2019-08-29 21:43:49 -0400

    Add player class!

---
+++ src/main/java/xyz/devosmium/games/mobiles/Player.java
@@ -0,0 +1,33 @@
+package xyz.devosmium.games.mobiles;
+
+import java.util.Map;
+
+public class Player extends Mobile {
+    private Player(int maxHealth, int health, String name, int level, Map<String, Integer> stats, int gold) {
+        super(maxHealth, health, name, level, stats, gold);
+        // TODO Auto-generated constructor stub
+    }
+
+	private static Player player;
+
+    public static Player getInstance() {
+        if (player == null) {
+            
+            player = new Player(15, 15, "player", 1, buildInitialStatBlock(), 0);
+        }
+
+        return player;
+    }
+
+    private static Map<String, Integer> buildInitialStatBlock() {
+        StatFactory factory = new StatFactory();
+
+        factory.setDex(5);
+        factory.setIntel(5);
+        factory.setLuck(5);
+        factory.setStr(5);
+        factory.setWis(5);
+
+        return factory.build();
+    }
+}
