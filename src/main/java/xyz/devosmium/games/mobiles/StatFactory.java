07f041cf12797882b4072eca0500a0c05cd2e9d3
Dev-Osmium <owen@devosmium.xyz>
2019-08-29 21:37:21 -0400

    Add Mobile class w/ stats enum and StatsFactory

---
+++ src/main/java/xyz/devosmium/games/mobiles/StatFactory.java
@@ -0,0 +1,32 @@
+package xyz.devosmium.games.mobiles;
+
+import java.util.Map;
+
+public class StatFactory {
+
+    private Map<String, Integer> stats = new Map<String, Integer>();
+
+    public Map<String, Integer> build() {
+        return this.stats;
+    }
+
+    public void setStr(int str) {
+        stats.put("str", str);
+    }
+
+    public void setDex(int dex) {
+        stats.put("dex", dex);
+    }
+
+    public void setLuck(int luck) {
+        stats.put("luck", luck); 
+    }
+
+    public void setIntel(int intel) {
+        stats.put("int", intel);
+    }
+
+    public void setWis(int wis) {
+        stats.put("wis", wis);
+    }
+}
---
+++ src/main/java/xyz/devosmium/games/mobiles/Stats.java
@@ -0,0 +1,9 @@
+package xyz.devosmium.games.mobiles;
+
+public enum Stats {
+    STRENGTH,
+    DEXTERITY,
+    INTELLIGENCE,
+    LUCK,
+    WISDOM;
+}
