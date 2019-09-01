c879e0392504cae15f66d3e80723d286b74003c3
Dev-Osmium <owen@devosmium.xyz>
2019-08-31 20:58:01 -0400

    Spotless

--- src/main/java/xyz/devosmium/games/textadventureengine/commands/Command.java
+++ src/main/java/xyz/devosmium/games/textadventureengine/commands/Command.java
@@ -2,6 +2,7 @@ package xyz.devosmium.games.textadventureengine.commands;
 
 public @interface Command {
 
-    String command();
-    String description();
+  String command();
+
+  String description();
 }
--- src/main/java/xyz/devosmium/games/textadventureengine/exceptions/DeathException.java
+++ src/main/java/xyz/devosmium/games/textadventureengine/exceptions/DeathException.java
@@ -2,14 +2,14 @@ package xyz.devosmium.games.textadventureengine.exceptions;
 
 public class DeathException extends Exception {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    public DeathException() {
-        super();
-    }
+  public DeathException() {
+    super();
+  }
 
-    public DeathException(String message) {
-        super(message);
-    }
+  public DeathException(String message) {
+    super(message);
+  }
 
 }
