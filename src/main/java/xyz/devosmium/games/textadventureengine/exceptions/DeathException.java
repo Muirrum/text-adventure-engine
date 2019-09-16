bfbf195889384d4f676c2e14c98d0c9b662be588
Dev-Osmium <owen@devosmium.xyz>
2019-09-16 08:43:17 -0400

    Javadocs and spotless

--- src/main/java/xyz/devosmium/games/textadventureengine/exceptions/DeathException.java
+++ src/main/java/xyz/devosmium/games/textadventureengine/exceptions/DeathException.java
@@ -1,5 +1,8 @@
 package xyz.devosmium.games.textadventureengine.exceptions;
 
+/**
+ * Thrown when the player dies.
+ */
 public class DeathException extends Exception {
 
   private static final long serialVersionUID = 1L;
--- src/main/java/xyz/devosmium/games/textadventureengine/locations/Location.java
+++ src/main/java/xyz/devosmium/games/textadventureengine/locations/Location.java
@@ -6,32 +6,49 @@ import xyz.devosmium.games.textadventureengine.mobiles.Mobile;
 import xyz.devosmium.games.textadventureengine.util.Coordinate;
 
 /**
- * 
+ * Represents a defined location in the world. Built by a LocationFactory
  */
 public class Location {
 
   private Coordinate coordinates;
 
+  /**
+   * @return The Coordinate object representing this Location's position in space
+   */
   public Coordinate getCoordinate() {
     return coordinates;
   }
 
   private String shortString;
 
+  /**
+   * @return The short name of the location
+   */
   public String getShort() {
     return shortString;
   }
 
+  /**
+   * Sets the name of the location
+   * 
+   * @param shortString The new name of the location
+   */
   public void setShort(String shortString) {
     this.shortString = shortString;
   }
 
   private String longString;
 
+  /**
+   * @return The long description of the location
+   */
   public String getLong() {
     return longString;
   }
 
+  /**
+   * @param longString The new long description of the location
+   */
   public void setLong(String longString) {
     this.longString = longString;
   }
@@ -42,6 +59,10 @@ public class Location {
     return mobiles;
   }
 
+  /**
+   * @param mobiles An ArrayList of Mobile Entities
+   * @see Location(Coordinate, String, String)
+   */
   public Location(Coordinate coordinates, String shortString, String longString, ArrayList<Mobile> mobiles) {
     this.coordinates = coordinates;
     this.shortString = shortString;
@@ -49,12 +70,22 @@ public class Location {
     this.mobiles = mobiles;
   }
 
+  /**
+   * @param coordinates The coordinates
+   * @param shortString Name
+   * @param longString Long description
+   */
   public Location(Coordinate coordinates, String shortString, String longString) {
     this.coordinates = coordinates;
     this.shortString = shortString;
     this.longString = longString;
   }
 
+  /**
+   * Creates a default room in the center of the map. Not recommended
+   * 
+   * @deprecated
+   */
   public Location() {
     this.coordinates = new Coordinate(0, 0, 0);
     this.shortString = "Development Room";
