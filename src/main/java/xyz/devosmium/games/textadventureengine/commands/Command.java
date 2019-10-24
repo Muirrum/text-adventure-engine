840a1c4a853dbbcdb648c028bc37374dd94adfd1
Nouni EL Bachir <nouni.elbachir@gmail.com>
2019-10-24 01:26:16 +0100

    Add missing meta-annotations on the Command annotation interface.
    If not done, this annotation will never be gotten using reflexion in the
    EngineCommandRepository

--- src/main/java/xyz/devosmium/games/textadventureengine/commands/Command.java
+++ src/main/java/xyz/devosmium/games/textadventureengine/commands/Command.java
@@ -1,5 +1,12 @@
 package xyz.devosmium.games.textadventureengine.commands;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Command {
 
   String command();
